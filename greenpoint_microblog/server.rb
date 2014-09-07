1a3d745d6469918f77553e3846b32d72a7bac598
HeidiW <foywilliams@gmail.com>
2014-09-06 23:26:06 -0400

    Author handles and tag editing added

--- greenpoint_microblog/server.rb
+++ greenpoint_microblog/server.rb
@@ -12,7 +12,7 @@ after do
 end
 
 get("/") do
-  erb(:index)
+  erb(:index, { locals: { posts: Post.all() } })
 end
 
 get("/authors") do 
@@ -22,9 +22,15 @@ get("/authors") do
 end
 
 get("/authors/:id") do
-  author = Author.find_by({id: params[:id]})
+  author = Author.find(params[:id])
   erb(:author, { locals: { author: author } })
 end
+
+get("/authors/:authorhandle/posts") do
+  author = Author.find_by({handle: params[:authorhandle]})
+  erb(:author, { locals: { author: author } })
+end
+
 
 # get("/authors/new") do
 # 	binding.pry
@@ -36,17 +42,40 @@ get("/posts/new") do
   erb(:new_post, { locals: { authors: Author.all } })
 end
 
+get("/posts/:id") do
+  post = Post.find(params[:id])
+  erb(:post, { locals: { post: post } })
+end
+
 post("/posts") do
+
+  if params["author_id"] == "new"
+    name = params["name"]
+    handle = params["handle"]
+    author = Author.create(name: name, handle: handle)
+    author_id = author[:id]
+  else
+    author_id = params["author_id"]
+  end
+
   post_hash = {
     tag: params["tag"],
     headline: params["headline"],
     copy: params["copy"],
-    author_id: params["author_id"],
+    author_id: author_id,
     created_at: Time.now
   }
 
-  Post.create(post_hash)
-  erb(:posts, { locals: { posts: Post.all() } })
+  post = Post.create(post_hash)
+  erb(:post, { locals: { post: post } })
+end
+
+post("/posts/:id") do 
+  post = Post.find(params[:id])
+  tag = params[:tag]
+  post.update(tag: tag)
+  @tag_updated = true
+  erb(:post, { locals: { post: post } })
 end
 
 post("/subscriber/new") do
